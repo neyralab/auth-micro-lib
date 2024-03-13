fd26d6b966bb36c1d004cc125f2363a8e866a71e
SvirskyiAndrii <a.svirskyi@ghostdrive.com>
2024-03-13 09:32:06 +0200

    add web3 modal wagmi

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { loginMetamask } from './loginMetamask';
 export { loginEmail } from './loginEmail';
+export { default as Web3ModalProvider } from './walletConnect/Web3ModalProvider';
+export { useWeb3Modal, useWeb3ModalEvents } from '@web3modal/wagmi/react';
+export { useAccount, useDisconnect, useSignMessage } from 'wagmi';
