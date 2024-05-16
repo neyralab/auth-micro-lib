3307323393d0234df3e6a8e7f147cc0f5c4bce13
SvirskyiAndrii <a.svirskyi@ghostdrive.com>
2024-05-16 15:36:15 +0300

    add connectNewUser to localStorage

--- src/utils/redirectionAfterLogin.ts
+++ src/utils/redirectionAfterLogin.ts
@@ -2,7 +2,9 @@ export const redirectionAfterLogin = (isNewUser: boolean) => {
   const redirectPath = isNewUser ? '/welcome' : '/chat';
   const redirectHref = `${window.location.origin}${redirectPath}`;
   const previousSpace = document.referrer.includes('space');
-  
+
+  isNewUser && window.localStorage.setItem('connectNewUser', 'true');
+
   if (previousSpace && isNewUser) {
     window.localStorage.setItem('referrer', document.referrer);
   }
