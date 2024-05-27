8908829d222df90e6456e8af2e3981f9985d8be5
SvirskyiAndrii <a.svirskyi@ghostdrive.com>
2024-05-27 11:21:07 +0300

    fix redirectionAfterLogin

--- src/utils/redirectionAfterLogin.ts
+++ src/utils/redirectionAfterLogin.ts
@@ -1,12 +1,21 @@
 export const redirectionAfterLogin = (isNewUser: boolean) => {
   const redirectPath = isNewUser ? '/welcome' : '/chat';
-  const redirectHref = `${window.location.origin}${redirectPath}`;
   const previousSpace = document.referrer.includes('space');
+  const redirectHref = `${
+    previousSpace ? removeTrailingSlash(document.referrer) : window.location.origin
+  }${redirectPath}`;
 
   isNewUser && window.localStorage.setItem('connectNewUser', 'true');
 
   if (previousSpace && isNewUser) {
     window.localStorage.setItem('referrer', document.referrer);
   }
-  window.location.href = isNewUser ? redirectHref : previousSpace ? document.referrer : redirectHref;
+  window.location.href = redirectHref;
+};
+
+const removeTrailingSlash = (url: string) => {
+  if (url.endsWith('/')) {
+    return url.slice(0, -1);
+  }
+  return url;
 };
